8c532c1897dfcad1772a7cf7d6457fb6fd549aac
Vinicius Davi <viniprogrammer@hotmail.com>
2023-02-23 13:24:26 -0300

    Create Ex034.c

---
+++ Exercicios_em_C/Ex034.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+    int i, j, acertou = 0, a, b, soma, cont = 1;
+    int vet[5];
+
+    for(i = 0; i < 5; i++){
+        a = rand() % 101;
+        b = rand() % 101;
+        vet[i] = a + b;
+        
+        printf("Qual é a soma entre %d + %d\n", a, b);
+        scanf("%d", &soma);
+
+        if(soma == (a + b)){
+            acertou++;
+        }
+    }
+
+    printf("\nVocê obteve %d acertos!\n\n", acertou);
+
+    for(i = 0; i < 5; i++){
+        printf("A soma da questão %d valia = %d\n", cont, vet[i]);
+        cont ++;
+    }
+
+    return 0;
+}
